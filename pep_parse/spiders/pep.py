a1493909b83a015ed3f39e564ed7732706979ece
sof07 <64215063+sof07@users.noreply.github.com>
2024-01-09 00:15:16 +0300

    Написан парсер для сбора информации

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -7,4 +7,13 @@ class PepSpider(scrapy.Spider):
     start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        all_peps = response.css('a[href^="pep-"]')
+        for group in all_peps:
+            yield response.follow(group, callback=self.parse_group)
+
+    def parse_group(self, response):
+        pep_title = response.css('h1.page-title::text').get()
+        pep_number, pep_name = pep_title.split('–', 1)
+        yield {'num': pep_number.replace('PEP', '').strip(),
+               'name': pep_name.strip(),
+               'status': response.css('abbr::text').get()}
